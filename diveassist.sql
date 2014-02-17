c81fee507349741116dcae17bdc53092473193e1
Mark Cianciulli <mark@gogreenhouse.com>
2014-02-17 15:26:13 -0500

    db 7

--- diveassist.sql
+++ diveassist.sql
@@ -16,6 +16,28 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `check`
+--
+
+DROP TABLE IF EXISTS `check`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `check` (
+  `check` int(11) NOT NULL,
+  PRIMARY KEY (`check`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `check`
+--
+
+LOCK TABLES `check` WRITE;
+/*!40000 ALTER TABLE `check` DISABLE KEYS */;
+/*!40000 ALTER TABLE `check` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `test`
 --
 
@@ -75,4 +97,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-02-17 15:23:43
+-- Dump completed on 2014-02-17 15:25:52
